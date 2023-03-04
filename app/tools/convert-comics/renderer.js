b12321b445c50e3f299c7e71b065f1f79118b3f7
Álvaro García <alvaro@binarynonsense.com>
2023-03-04 01:22:57 +0100

    Fix conversion bug with some pdf files
    The latest version of pdfjs fixes it but it seems slower, so I use it only for the failed pages

--- app/tools/convert-comics/renderer.js
+++ app/tools/convert-comics/renderer.js
@@ -370,105 +370,222 @@ ipcRenderer.on(g_ipcChannel + "show-result", (event) => {
 // PDF ////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
-const pdfjsLib = require("../../assets/libs/pdfjs/build/pdf.js");
+const pdfjsFolderName_1 = "pdfjs"; // 2.3.200
+const pdfjsFolderName_2 = "pdfjs-3.4.120";
 
 async function extractPDFImages(folderPath, logText, password) {
   try {
-    // ref: https://kevinnadro.com/blog/parsing-pdfs-in-javascript/
-    pdfjsLib.GlobalWorkerOptions.workerSrc =
-      "../../assets/libs/pdfjs/build/pdf.worker.js";
-    //pdfjsLib.disableWorker = true;
-    const pdf = await pdfjsLib.getDocument({
-      url: g_inputFilePath,
-      password: password,
-    }).promise;
-    for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
-      if (g_cancel) {
-        pdf.cleanup();
-        pdf.destroy();
-        ipcRenderer.send(g_ipcChannel + "pdf-images-extracted", true);
-        return;
-      }
-      let page = await pdf.getPage(pageNum);
-      let pageWidth = page.view[2]; // [left, top, width, height]
-      let pageHeight = page.view[3];
-      let userUnit = page.userUnit; // 1 unit = 1/72 inch
-      let dpi = 300; // use userUnit some day (if > 1) to set dpi?
-      let iPerUnit = 1 / 72;
-      let scaleFactor = dpi * iPerUnit; // default: output a 300dpi image instead of 72dpi, which is the pdf default?
-      if (g_outputPdfExtractionMethod === "render72") {
-        scaleFactor = 1;
-        dpi = 72;
-      }
-      // resize if too big?
-      let bigSide = pageHeight;
-      if (pageHeight < pageWidth) bigSide = pageWidth;
-      let scaledSide = bigSide * scaleFactor;
-      if (scaledSide > 5000) {
-        console.log("reducing PDF scale factor, img too big");
-        scaleFactor = 5000 / bigSide;
-        dpi = parseInt(scaleFactor / iPerUnit);
-      }
-      // RENDER
-      const canvas = document.createElement("canvas");
-      let viewport = page.getViewport({
-        scale: scaleFactor,
-      });
-      let context = canvas.getContext("2d");
-      canvas.height = viewport.height;
-      canvas.width = viewport.width;
-      await page.render({ canvasContext: context, viewport: viewport }).promise;
-      ////////////////////////////
-      if (g_outputPdfExtractionMethod === "embedded") {
-        // check imgs size
-        // ref: https://codepen.io/allandiego/pen/RwVGbyj
-        const operatorList = await page.getOperatorList();
-        const validTypes = [
-          pdfjsLib.OPS.paintImageXObject,
-          pdfjsLib.OPS.paintJpegXObject,
-          //pdfjsLib.OPS.paintImageXObjectRepeat,
-        ];
-        let images = [];
-        operatorList.fnArray.forEach((element, index) => {
-          if (validTypes.includes(element)) {
-            images.push(operatorList.argsArray[index][0]);
-          }
+    // FIRST PASS
+    // Uses an older pdf library, it's faster but has a bug in page.obj.get that fails for some files
+    let pdfjsFolderName = pdfjsFolderName_1;
+    let failedPages = [];
+    {
+      const pdfjsLib = require(`../../assets/libs/${pdfjsFolderName}/build/pdf.js`);
+      // ref: https://kevinnadro.com/blog/parsing-pdfs-in-javascript/
+      pdfjsLib.GlobalWorkerOptions.workerSrc = `../../assets/libs/${pdfjsFolderName}/build/pdf.worker.js`;
+      //pdfjsLib.disableWorker = true;
+      const pdf = await pdfjsLib.getDocument({
+        url: g_inputFilePath,
+        password: password,
+      }).promise;
+      for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
+        if (g_cancel) {
+          pdf.cleanup();
+          pdf.destroy();
+          ipcRenderer.send(g_ipcChannel + "pdf-images-extracted", true);
+          return;
+        }
+        let page = await pdf.getPage(pageNum);
+        let pageWidth = page.view[2]; // [left, top, width, height]
+        let pageHeight = page.view[3];
+        let userUnit = page.userUnit; // 1 unit = 1/72 inch
+        let dpi = 300; // use userUnit some day (if > 1) to set dpi?
+        let iPerUnit = 1 / 72;
+        let scaleFactor = dpi * iPerUnit; // default: output a 300dpi image instead of 72dpi, which is the pdf default?
+        if (g_outputPdfExtractionMethod === "render72") {
+          scaleFactor = 1;
+          dpi = 72;
+        }
+        // resize if too big?
+        let bigSide = pageHeight;
+        if (pageHeight < pageWidth) bigSide = pageWidth;
+        let scaledSide = bigSide * scaleFactor;
+        if (scaledSide > 5000) {
+          console.log("reducing PDF scale factor, img too big");
+          scaleFactor = 5000 / bigSide;
+          dpi = parseInt(scaleFactor / iPerUnit);
+        }
+        // RENDER
+        const canvas = document.createElement("canvas");
+        let viewport = page.getViewport({
+          scale: scaleFactor,
         });
-        if (images.length === 1) {
-          // could be a comic book, let's extract the image
-          const imageName = images[0];
-          // page needs to have been rendered before for this to be filled
-          let image = await page.objs.get(imageName);
-          const imageWidth = image.width;
-          const imageHeight = image.height;
-          if (imageWidth >= pageWidth && imageHeight >= pageHeight) {
-            scaleFactor = imageWidth / pageWidth;
-            dpi = parseInt(scaleFactor / iPerUnit);
-            // render again with new dimensions
-            viewport = page.getViewport({
-              scale: scaleFactor,
-            });
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
-            await page.render({ canvasContext: context, viewport: viewport })
-              .promise;
+        let context = canvas.getContext("2d");
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        await page.render({ canvasContext: context, viewport: viewport })
+          .promise;
+        ////////////////////////////
+        if (g_outputPdfExtractionMethod === "embedded") {
+          // check imgs size
+          // ref: https://codepen.io/allandiego/pen/RwVGbyj
+          const operatorList = await page.getOperatorList();
+          const validTypes = [
+            pdfjsLib.OPS.paintImageXObject,
+            pdfjsLib.OPS.paintJpegXObject,
+            //pdfjsLib.OPS.paintImageXObjectRepeat,
+          ];
+          let images = [];
+          operatorList.fnArray.forEach((element, index) => {
+            if (validTypes.includes(element)) {
+              images.push(operatorList.argsArray[index][0]);
+            }
+          });
+          if (images.length === 1) {
+            // could be a comic book, let's extract the image
+            const imageName = images[0];
+            try {
+              // page needs to have been rendered before for this to be filled
+              let image = await page.objs.get(imageName);
+              const imageWidth = image.width;
+              const imageHeight = image.height;
+              if (imageWidth >= pageWidth && imageHeight >= pageHeight) {
+                scaleFactor = imageWidth / pageWidth;
+                dpi = parseInt(scaleFactor / iPerUnit);
+                // render again with new dimensions
+                viewport = page.getViewport({
+                  scale: scaleFactor,
+                });
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+                await page.render({
+                  canvasContext: context,
+                  viewport: viewport,
+                }).promise;
+              }
+            } catch (error) {
+              failedPages.push(pageNum);
+              continue;
+            }
           }
         }
+        //////////////////////////////
+        let dataUrl = canvas.toDataURL("image/jpeg", 0.8);
+        let img = changeDpiDataUrl(dataUrl, dpi);
+        let data = img.replace(/^data:image\/\w+;base64,/, "");
+        let buf = Buffer.from(data, "base64");
+
+        let filePath = path.join(folderPath, pageNum + "." + FileExtension.JPG);
+        fs.writeFileSync(filePath, buf, "binary");
+        updateLogText(logText + pageNum + " / " + pdf.numPages);
+
+        page.cleanup();
       }
-      //////////////////////////////
-      let dataUrl = canvas.toDataURL("image/jpeg", 0.8);
-      let img = changeDpiDataUrl(dataUrl, dpi);
-      let data = img.replace(/^data:image\/\w+;base64,/, "");
-      let buf = Buffer.from(data, "base64");
-
-      let filePath = path.join(folderPath, pageNum + "." + FileExtension.JPG);
-      fs.writeFileSync(filePath, buf, "binary");
-      updateLogText(logText + pageNum + " / " + pdf.numPages);
-
-      page.cleanup();
+      pdf.cleanup();
+      pdf.destroy();
     }
-    pdf.cleanup();
-    pdf.destroy();
+    // SECOND PASS
+    // Try to extract failed pages using newer but slower version of the library
+    pdfjsFolderName = pdfjsFolderName_2;
+    if (failedPages.length > 0) {
+      console.log("retrying pages: " + failedPages);
+      const pdfjsLib = require(`../../assets/libs/${pdfjsFolderName}/build/pdf.js`);
+      // ref: https://kevinnadro.com/blog/parsing-pdfs-in-javascript/
+      pdfjsLib.GlobalWorkerOptions.workerSrc = `../../assets/libs/${pdfjsFolderName}/build/pdf.worker.js`;
+      //pdfjsLib.disableWorker = true;
+      const pdf = await pdfjsLib.getDocument({
+        url: g_inputFilePath,
+        password: password,
+      }).promise;
+      for (pageNum of failedPages) {
+        if (g_cancel) {
+          pdf.cleanup();
+          pdf.destroy();
+          ipcRenderer.send(g_ipcChannel + "pdf-images-extracted", true);
+          return;
+        }
+        let page = await pdf.getPage(pageNum);
+        let pageWidth = page.view[2]; // [left, top, width, height]
+        let pageHeight = page.view[3];
+        let userUnit = page.userUnit; // 1 unit = 1/72 inch
+        let dpi = 300; // use userUnit some day (if > 1) to set dpi?
+        let iPerUnit = 1 / 72;
+        let scaleFactor = dpi * iPerUnit; // default: output a 300dpi image instead of 72dpi, which is the pdf default?
+        if (g_outputPdfExtractionMethod === "render72") {
+          scaleFactor = 1;
+          dpi = 72;
+        }
+        // resize if too big?
+        let bigSide = pageHeight;
+        if (pageHeight < pageWidth) bigSide = pageWidth;
+        let scaledSide = bigSide * scaleFactor;
+        if (scaledSide > 5000) {
+          console.log("reducing PDF scale factor, img too big");
+          scaleFactor = 5000 / bigSide;
+          dpi = parseInt(scaleFactor / iPerUnit);
+        }
+        // RENDER
+        const canvas = document.createElement("canvas");
+        let viewport = page.getViewport({
+          scale: scaleFactor,
+        });
+        let context = canvas.getContext("2d");
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        await page.render({ canvasContext: context, viewport: viewport })
+          .promise;
+        ////////////////////////////
+        if (g_outputPdfExtractionMethod === "embedded") {
+          // check imgs size
+          // ref: https://codepen.io/allandiego/pen/RwVGbyj
+          const operatorList = await page.getOperatorList();
+          const validTypes = [
+            pdfjsLib.OPS.paintImageXObject,
+            pdfjsLib.OPS.paintJpegXObject,
+            //pdfjsLib.OPS.paintImageXObjectRepeat,
+          ];
+          let images = [];
+          operatorList.fnArray.forEach((element, index) => {
+            if (validTypes.includes(element)) {
+              images.push(operatorList.argsArray[index][0]);
+            }
+          });
+          if (images.length === 1) {
+            // could be a comic book, let's extract the image
+            const imageName = images[0];
+            // page needs to have been rendered before for this to be filled
+            let image = await page.objs.get(imageName);
+            const imageWidth = image.width;
+            const imageHeight = image.height;
+            if (imageWidth >= pageWidth && imageHeight >= pageHeight) {
+              scaleFactor = imageWidth / pageWidth;
+              dpi = parseInt(scaleFactor / iPerUnit);
+              // render again with new dimensions
+              viewport = page.getViewport({
+                scale: scaleFactor,
+              });
+              canvas.height = viewport.height;
+              canvas.width = viewport.width;
+              await page.render({ canvasContext: context, viewport: viewport })
+                .promise;
+            }
+          }
+        }
+        //////////////////////////////
+        let dataUrl = canvas.toDataURL("image/jpeg", 0.8);
+        let img = changeDpiDataUrl(dataUrl, dpi);
+        let data = img.replace(/^data:image\/\w+;base64,/, "");
+        let buf = Buffer.from(data, "base64");
+
+        let filePath = path.join(folderPath, pageNum + "." + FileExtension.JPG);
+        fs.writeFileSync(filePath, buf, "binary");
+        updateLogText(logText + pageNum + " / " + pdf.numPages);
+
+        page.cleanup();
+      }
+      pdf.cleanup();
+      pdf.destroy();
+    }
     ipcRenderer.send(g_ipcChannel + "pdf-images-extracted", false);
   } catch (err) {
     ipcRenderer.send(g_ipcChannel + "stop-error", err);
