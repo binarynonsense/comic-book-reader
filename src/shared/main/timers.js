6545e99490f45dd450b46d8bf8b4d35227d4d1e1
Álvaro García <alvaro@binarynonsense.com>
2023-09-16 19:30:06 +0200

    Add extra safety checks

--- src/shared/main/timers.js
+++ src/shared/main/timers.js
@@ -8,10 +8,12 @@
 let g_timers = {};
 
 exports.start = function (label) {
+  if (!label || typeof label !== "string" || label.trim() === "") return;
   g_timers[label] = performance.now();
 };
 
 exports.stop = function (label) {
+  if (!label || typeof label !== "string" || label.trim() === "") return;
   const time = performance.now() - g_timers[label];
   delete g_timers[label];
   return (time / 1000).toFixed(3);
