1b24976c2b6a3291a5df69736c88de48d178fdeb
Álvaro García <alvaro@binarynonsense.com>
2024-06-02 11:40:28 +0200

    Allow dragging files into the home screen

--- src/reader/home-screen/renderer.js
+++ src/reader/home-screen/renderer.js
@@ -391,6 +391,12 @@ export function onInputEvent(type, event) {
         event.preventDefault();
       }
       break;
+
+    case "body.ondrop":
+      {
+        sendIpcToMain("open-file", event.dataTransfer.files[0].path);
+      }
+      break;
   }
 }
 
