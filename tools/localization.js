72c052f07886b8590a6c2c957a34211768894b44
Álvaro García <alvaro@binarynonsense.com>
2024-06-08 18:47:15 +0200

    Add tool to create localization file to the licenses folder

--- tools/localization.js
+++ tools/localization.js
@@ -13,6 +13,9 @@ switch (process.argv[2]) {
   case "state":
     updateLocalizationFiles(process.argv[2], process.argv[3]);
     break;
+  case "license":
+    updateLicenseFile();
+    break;
   default:
     console.log("operation parameter missing");
     return;
@@ -73,6 +76,40 @@ function updateLocalizationFiles(operation, languageId) {
         console.log("-----------------------------");
       }
     });
+  } catch (error) {
+    console.log("something went wrong");
+    console.log(error);
+  }
+}
+
+function updateLicenseFile() {
+  try {
+    const folderPath = "./src/assets/i18n/";
+    const folderContents = fs.readdirSync("./src/assets/i18n/");
+    let text = "Localization files:\n\n";
+    folderContents.forEach((entryName) => {
+      const entryPath = path.join(folderPath, entryName);
+      if (
+        !fs.lstatSync(entryPath).isDirectory() &&
+        entryName.endsWith(".json")
+      ) {
+        let entryData = JSON.parse(fs.readFileSync(entryPath, "utf8"));
+        const name = entryData["@metadata"]["name"];
+        const authorsArray = entryData["@metadata"]["authors"];
+        let authors = "";
+        for (let index = 0; index < authorsArray.length; index++) {
+          const author = authorsArray[index];
+          authors += author;
+          if (index < authorsArray.length - 1) {
+            authors += ",";
+          }
+        }
+        const license = entryData["@metadata"]["license-id"];
+        text += `language: ${name}\nauthors: ${authors}\nlicense: ${license}\n\n`;
+      }
+    });
+    console.log(text);
+    fs.writeFileSync("./licenses/localization.txt", text, "utf-8");
   } catch (error) {
     console.log("something went wrong");
     console.log(error);
