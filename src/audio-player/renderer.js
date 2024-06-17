955c49f27675efb8da19729de0dd46bc381725cc
Álvaro García <alvaro@binarynonsense.com>
2024-06-17 18:55:46 +0200

    Use monospace font in track names

--- src/audio-player/renderer.js
+++ src/audio-player/renderer.js
@@ -534,7 +534,7 @@ function onSliderVolumeChanged(slider) {
 }
 
 function getFormatedTimeFromSeconds(seconds) {
-  if (isNaN(seconds) || !isFinite(seconds)) return "";
+  if (isNaN(seconds) || !isFinite(seconds)) return "--:--";
   let hours = Math.floor(seconds / 3600);
   let minutes = Math.floor((seconds - hours * 3600) / 60);
   seconds = Math.floor(seconds - hours * 3600 - minutes * 60);
