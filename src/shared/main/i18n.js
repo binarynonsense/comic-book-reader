6eea7d68d46d85d2826ad876f5c68b36df28ff92
Álvaro García <alvaro@binarynonsense.com>
2024-06-22 13:03:53 +0200

    Remove no longer needed entries and rename others

--- src/shared/main/i18n.js
+++ src/shared/main/i18n.js
@@ -143,7 +143,7 @@ function getLocalesFromFolder(folderPath) {
               nativeName: data["@metadata"]["native-name"],
               locale: data["@metadata"]["locale"],
               acbrVersion: data["@metadata"]["acbr-version"],
-              outdatedText: data["tool-pre-languages-incompletelanguage"],
+              outdatedText: data["tool-pre-language-incomplete"],
             };
             localesList.push(localeInfo);
           }
