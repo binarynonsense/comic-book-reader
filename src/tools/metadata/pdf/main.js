ecd9a068e9f167f4403fe065fdcb9f669a569952
Álvaro García <alvaro@binarynonsense.com>
2024-07-01 21:28:48 +0200

    Add metadata producer fallbacks

--- src/tools/metadata/pdf/main.js
+++ src/tools/metadata/pdf/main.js
@@ -80,7 +80,13 @@ exports.saveMetadataToFile = async function (data) {
       }
       pdf.setKeywords(data["keywords"]); // must be array
     }
-    if (data["producer"]) pdf.setProducer(data["producer"]);
+    if (data["producer"]) {
+      pdf.setProducer(data["producer"]);
+    } else if (g_metadata.producer) {
+      pdf.setProducer(g_metadata.producer);
+    } else {
+      pdf.setProducer("ACBR");
+    }
     if (data["creator"]) pdf.setCreator(data["creator"]);
 
     if (data["creationDate"]) {
