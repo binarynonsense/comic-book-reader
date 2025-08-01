3b774fb4368107680a3db5a0cf4c944477c24358
Álvaro García <alvaro@binarynonsense.com>
2025-08-01 12:33:14 +0200

    Add toasts

---
+++ src/shared/renderer/toasts.js
@@ -0,0 +1,31 @@
+/**
+ * @license
+ * Copyright 2025 Álvaro García
+ * www.binarynonsense.com
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+export function show(...args) {
+  generic(...args);
+}
+
+function generic(text, duration, onClick) {
+  // ref: https://github.com/apvarun/toastify-js/blob/master/README.md
+  let toast = Toastify({
+    text,
+    // style: {
+    //   background: "linear-gradient(to right, #00b09b, #96c93d)",
+    // },
+    offset: {
+      x: 30,
+      y: 30,
+    },
+    close: true,
+    onClick: () => {
+      if (onClick) onClick();
+      toast.hideToast();
+    },
+    duration,
+    stopOnFocus: true,
+  }).showToast();
+}
